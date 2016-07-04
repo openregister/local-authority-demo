6c21bbd5c2e1434ab7372d5dc2b6f0c3530c2dbb
robmckinnon <rob@movingflow>
2016-07-04 11:33:25 +0100

    Retrieve data from file when offline
    When unable to retrieve data via HTTP, read data from local filesystem.

--- web/data/remote_data.ex
+++ web/data/remote_data.ex
@@ -9,7 +9,8 @@ defmodule RemoteData do
   def data_list kind do
     path = System.get_env("DATA_FILE")
     url = "https://raw.githubusercontent.com/openregister/#{path}"
-    ConCache.get_or_store(:my_cache, kind, get_list(url, kind))
+    file_path = "../#{path}" |> String.replace("/master","")
+    ConCache.get_or_store(:my_cache, kind, get_list(url, file_path, kind))
   end
 
   def maps_list do
@@ -22,27 +23,43 @@ defmodule RemoteData do
     ConCache.get_or_store(:my_cache, "index", get_maps_index)
   end
 
-  defp maps_url file do
+  defp maps_url_and_path file do
     path = System.get_env("MAPS_PATH")
-    "https://raw.githubusercontent.com/openregister/#{path}/#{file}"
+    url = "https://raw.githubusercontent.com/openregister/#{path}/#{file}"
+    file_path = "../#{path}/#{file}" |> String.replace("/master","")
+    [url, file_path]
   end
 
   defp map_list file do
-    url = maps_url "#{file}.tsv"
-    ConCache.get_or_store(:my_cache, file, get_list(url, file))
+    [url, file_path] = maps_url_and_path("#{file}.tsv")
+    ConCache.get_or_store(:my_cache, file, get_list(url, file_path, file))
   end
 
   defp get_maps_index do
+    [url, file_path] = maps_url_and_path("index.yml")
     fn () ->
-      url = maps_url "index.yml"
-      HTTPoison.get!(url).body
+      url
+      |> retrieve_data(file_path)
       |> YamlElixir.read_from_string
     end
   end
 
-  defp get_list url, kind do
-    fn () ->
+  defp retrieve_data url, file_path do
+    try do
+      IO.puts url
       HTTPoison.get!(url).body
+    rescue
+      HTTPoison.Error ->
+        IO.puts file_path
+        {:ok, body} = File.read(file_path)
+        body
+    end
+  end
+
+  defp get_list url, file_path, kind do
+    fn () ->
+      url
+      |> retrieve_data(file_path)
       |> String.replace_trailing("\n", "")
       |> DataMorph.structs_from_tsv(OpenRegister, kind)
       |> Enum.to_list
